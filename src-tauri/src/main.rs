6ae30bc65451bd93dd3478960a23787fb53d922f
Gustavo da Hora <gustadahora68@gmail.com>
2025-09-14 01:16:55 -0400

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,45 @@
 // src-tauri/src/main.rs
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod commands;
+mod models;
+mod utils;
+mod errors;
+mod checks;
+
+use commands::{
+    create_document, open_document, save_document, save_document_as, 
+    create_backup, list_backups, restore_backup, export_document,
+    get_recent_files, validate_path
+};
+
+#[tauri::command]
+fn ping() -> String {
+    "pong".into()
+}
+
 fn main() {
-    clareza_lib::run();
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![
+            create_document,
+            open_document,
+            save_document,
+            save_document_as,
+            create_backup,
+            list_backups,
+            restore_backup,
+            export_document,
+            get_recent_files,
+            validate_path,
+            ping,
+            checks::check_node,
+            checks::check_npm,
+            checks::check_gemini
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
